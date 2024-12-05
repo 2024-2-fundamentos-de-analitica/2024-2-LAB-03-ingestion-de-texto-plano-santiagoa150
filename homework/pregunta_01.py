5cd424edb9e09c54a301ace23a12e1c475c73828
Santiago Álvarez Muñoz <santiagoa150@gmail.com>
2024-12-05 03:41:35 -0500

    feat: lab completed

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,9 +1,49 @@
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
+import glob
+import fileinput
+import pandas as pd
 
-# pylint: disable=import-outside-toplevel
 
+def load_data(input_file: str):
+    file = glob.glob(input_file)
+    lines = []
+    with fileinput.input(files=file) as f:
+        for line_number, line in enumerate(f, start=1):
+          if line_number > 4:
+            lines.append(line.strip()) 
+    return lines 
+
+def clean_data(lines):
+    processed = []
+
+    cluster = None
+    cantidad = None
+    porcentaje = None
+    palabras_clave = []
+    
+    for line in lines:
+      line = line.strip()
+      if line == "":
+        continue
+
+      if line.split()[0].isdigit():
+        if cluster is not None:
+          processed.append([cluster, cantidad, porcentaje, " ".join(palabras_clave)])
+        # Procesa la nueva línea
+        partes = line.split(maxsplit=4)
+        cluster = int(partes[0])
+        cantidad = int(partes[1])
+        porcentaje = float(partes[2].replace(",", "."))
+        palabras_clave = [partes[4]]
+      else:
+        palabras_clave.append(line)
+
+    if cluster is not None:
+      processed.append([cluster, cantidad, porcentaje, " ".join(palabras_clave)])
+
+    return processed
 
 def pregunta_01():
     """
@@ -18,3 +58,9 @@ def pregunta_01():
 
 
     """
+    lines = load_data('files/input/clusters_report.txt')
+    data = clean_data(lines)
+    df = pd.DataFrame(data, columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: x.split(','))
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: ', '.join([' '.join(word.replace('.', '').split()) for word in x]))
+    return df
